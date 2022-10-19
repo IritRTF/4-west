c6b8a692de77b5937329c149a703efae5bb3ac0d
Denis-GM <gainutdinov.deniz@yandex.ru>
2022-10-20 00:04:32 +0500

    6/10

--- src/index.js
+++ src/index.js
@@ -65,7 +65,6 @@ class Trasher extends Dog{
         this.currentPower = currentPower;
     }
 
-    //Правильно ли работает?
     modifyTakenDamage(value, fromCard, gameContext, continuation) {
         if (value - 1 != 0)
             this.view.signalAbility(() => {continuation(value - 1);})
@@ -74,7 +73,7 @@ class Trasher extends Dog{
     }
 }
 
-
+// 5 задание
 class Gatling extends Creature{
     constructor(name = "Гатлинг", currentPower = 6){
         super();
@@ -82,30 +81,150 @@ class Gatling extends Creature{
         this.maxPower = currentPower;
         this.currentPower = currentPower;
     }
+
+    attack(gameContext, continuation) {
+        const taskQueue = new TaskQueue();
+
+        const {currentPlayer, oppositePlayer, position, updateView} = gameContext;
+
+        taskQueue.push(onDone => this.view.showAttack(onDone));
+        const oppositeCards = oppositePlayer.table;
+        for(let i = 0; i < oppositeCards.length; i++){
+            let oppositeCard = oppositeCards[i];
+            taskQueue.push(onDone => {
+                if (oppositeCard) {
+                    this.dealDamageToCreature(this.currentPower, oppositeCard, gameContext, onDone);
+                }
+                else {
+                    onDone();
+                }
+            });
+        }
+        taskQueue.continueWith(continuation);
+    }
 }
 
+// 6 задание
+class Lad extends Dog {
+    constructor(name = "Браток", currentPower = 2){
+        super();
+        this.name = name;
+        this.maxPower = currentPower;
+        this.currentPower = currentPower;
+    }
+    
+    static getInGameCount() {
+        return this.inGameCount; // || 0; 
+    }
 
-const seriffStartDeck = [
-    new Duck(),
-    new Duck(),
-    new Duck(),
-    new Gatling(),
-];
-const banditStartDeck = [
-    new Trasher(),
-    new Dog(),
-    new Dog(),
-];
+    static setInGameCount(value) {
+        this.inGameCount = value;
+    }
 
-// // Колода Шерифа, нижнего игрока.
+    // Позволяет определять способности, которые должны активироваться при входе в игру.
+    doAfterComingIntoPlay(gameContext, continuation) {
+        const {currentPlayer, oppositePlayer, position, updateView} = gameContext;
+        Lad.inGameCount = (Lad.inGameCount > 0) ? Lad.inGameCount + 1: 1;
+        continuation();
+    };
+
+    // Позволяет определять способности, которые должны активироваться или завершаться при выходе карты из игры.
+    doBeforeRemoving(continuation) {
+        Lad.inGameCount = Lad.inGameCount - 1;
+        continuation();
+    };
+
+    static getBonus() {
+        // количество * (количество + 1) / 2
+        let count = Lad.inGameCount;
+        return count * (count + 1) / 2;
+    }
+
+    modifyDealedDamageToCreature(value, toCard, gameContext, continuation) {
+        let bonus = Lad.getBonus();
+        console.log(bonus)
+        continuation(value + bonus);
+    };
+
+    modifyTakenDamage(value, fromCard, gameContext, continuation) {
+        console.log(Lad.getBonus())
+        continuation(value - Lad.getBonus());
+    };
+
+    getDescriptions(){
+        let descriptions = super.getDescriptions();
+        if(Lad.prototype.hasOwnProperty('modifyDealedDamageToCreature') || Lad.prototype.hasOwnProperty('modifyTakenDamage')){
+            return ['«Чем их больше, тем они сильнее»', descriptions[0], descriptions[1]];
+        }
+        return [getInheritanceDescription(this), descriptions[0], descriptions[1]];
+    }
+
+    // getDescriptions() {
+    //     if(Lad.prototype.hasOwnProperty('modifyDealedDamageToCreature') || Lad.prototype.hasOwnProperty('modifyTakenDamage')){
+    //         return [getInheritanceDescription(this)];
+    //     }
+    //     return [getInheritanceDescription(this)];
+    // };
+}
+
+//Не вызывается из Card
+function getInheritanceDescription (card) {
+    const names = [];
+    let obj = card;
+    while (true) {
+        obj = Object.getPrototypeOf(obj);
+        names.push(obj.constructor.name);
+        if (obj === Card.prototype)
+            break;
+    }
+    return names.join('➔ ');
+}
+
+// 7 задание
+
+// //Проверка 3
 // const seriffStartDeck = [
 //     new Duck(),
 //     new Duck(),
 //     new Duck(),
 // ];
+// const banditStartDeck = [
+//     new Dog(),
+// ];
 
+//Проверка 4
+// const seriffStartDeck = [
+//     new Duck(),
+//     new Duck(),
+//     new Duck(),
+//     new Duck(),
+// ];
 // const banditStartDeck = [
 //     new Trasher(),
+// ];
+
+// //Проверка 5
+// const seriffStartDeck = [
+//     new Duck(),
+//     new Duck(),
+//     new Duck(),
+//     new Gatling(),
+// ];
+// const banditStartDeck = [
+//     new Trasher(),
+//     new Dog(),
+//     new Dog(),
+// ];
+
+// // Проверка 6
+// const seriffStartDeck = [
+//     new Duck(),
+//     new Duck(),
+//     new Duck(),
+// ];
+// const banditStartDeck = [
+//     new Lad(),
+//     new Lad(),
 // ];
 
 // Создание игры.
@@ -118,6 +237,3 @@ SpeedRate.set(1);
 game.play(false, (winner) => {
     alert('Победил ' + winner.name);
 });
-
-
-
