e6fac1fadbe54a779aa8c515e8299fcf2f9f79e8
IuxMorti <zdracte21@bk.ru>
2022-10-30 21:39:15 +0500

    done task 2-3

--- src/index.js
+++ src/index.js
@@ -27,30 +27,54 @@ function getCreatureDescription(card) {
     return 'Существо';
 }
 
+class Creature extends Card{
+    constructor(name = null, power = 0, image = null){
+        super(name, power, image)
+    }
 
+    getDescriptions(){
+        return [getCreatureDescription(this), ...super.getDescriptions()]
+    }
+}
 
 // Основа для утки.
-function Duck() {
-    this.quacks = function () { console.log('quack') };
-    this.swims = function () { console.log('float: both;') };
+class Duck extends Creature {
+    constructor(name = "Мирная утка", power = 2, image = null ) {
+        super(name, power, image)
+    }
+    
+    quacks() {
+        console.log('quack');
+    }
+    
+    swims () {
+        console.log('float: both;');
+    };
 }
 
 
 // Основа для собаки.
-function Dog() {
+class Dog extends Creature{
+    constructor(name = "Пес-бандит", power = 3, image = null){
+        super(name, power, image)
+    }
 }
+
+
+
+
 
 
 // Колода Шерифа, нижнего игрока.
 const seriffStartDeck = [
-    new Card('Мирный житель', 2),
-    new Card('Мирный житель', 2),
-    new Card('Мирный житель', 2),
+    new Duck(),
+    new Duck(),
+    new Duck(),
 ];
 
 // Колода Бандита, верхнего игрока.
 const banditStartDeck = [
-    new Card('Бандит', 4),
+    new Dog(),
 ];
 
 
