abe9a7f4615978e4933f98570e604ec71ff09b6c
Eggier <105437746+Eggier@users.noreply.github.com>
2022-12-15 16:03:50 +0500

    Update TaskQueue.js

--- src/TaskQueue.js
+++ src/TaskQueue.js
@@ -32,24 +32,18 @@ class TaskQueue {
     taskQueue.running = true;
     const task = taskQueue.tasks.shift()
 
-        if (task.runAndContinue) {
-            setTimeout(() => {
-                task.runAndContinue(() => {
-                    task.dispose && task.dispose();
-                    taskQueue.running = false;
+     if (task.runAndContinue) {
+         setTimeout(() => {
+             task.runAndContinue(() => {
+                 task.dispose && task.dispose();
+                 taskQueue.running = false;
 
-                    setTimeout(() => {
-                        runNextTask(taskQueue);
-                    });
-                });
-            }, 0);
-        }
-        else {
-            runNextTask(taskQueue);
-        }
+                 setTimeout(() => {
+                     runNextTask(taskQueue);
+                 });
+            });
+        }, 0);
+    } else {
+        runNextTask(taskQueue);
     }
-
-    return TaskQueue;
-}();
-
 export default TaskQueue;
