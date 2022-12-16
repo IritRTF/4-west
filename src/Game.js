a8a18189e1ab6dbed3693e156c89e8a49bbf49e2
Eggier <105437746+Eggier@users.noreply.github.com>
2022-12-16 15:31:19 +0500

    Update Game.js

--- src/Game.js
+++ src/Game.js
@@ -1,14 +1,14 @@
 import Player from './Player.js';
 import PlayerView from './PlayerView.js';
 
-const Game = function() {
-    function Game(bottomPlayerDeck, topPlayerDeck) {
+class Game {
+    constructor(bottomPlayerDeck, topPlayerDeck) {
         this.bottomPlayerStartDeck = bottomPlayerDeck;
         this.topPlayerStartDeck = topPlayerDeck;
     }
 
     // Подготавливает колоды, создает игроков, запускает игру.
-    Game.prototype.play = function (needShuffleDecks, onGameOver) {
+    play(needShuffleDecks, onGameOver) {
         const bottomPlayerDeck = needShuffleDecks
             ? copyAndShuffle(this.bottomPlayerStartDeck)
             : copyAndReverse(this.bottomPlayerStartDeck);
@@ -35,39 +35,7 @@ const Game = function() {
         this.oppositePlayer = bottomPlayer;
 
         playStaged(this, 0, onGameOver);
-    };
-
-    // Выполняет действия для некоторой стадии хода.
-    // Переход к следующей стадии идет через самовызов в колбэке.
-    function playStaged(game, stage, onGameOver) {
-        switch (stage) {
-            case 0:
-                game.currentPlayer.playNewCard(() => playStaged(game, 1, onGameOver));
-                break;
-            case 1:
-                game.currentPlayer.applyCards(() => playStaged(game, 2, onGameOver));
-                break;
-            case 2:
-                game.oppositePlayer.removeDeadAndCompactTable(() => playStaged(game, 3, onGameOver));
-                break;
-            case 3:
-                game.currentPlayer.removeDeadAndCompactTable(() => playStaged(game, 4, onGameOver));
-                break;
-            case 4:
-                const winner = getWinner(game);
-                if (winner) {
-                    onGameOver(winner);
-                    return;
-                }
-                playStaged(game, 5, onGameOver);
-                break;
-            case 5:
-                changePlayer(game);
-                playStaged(game, 0, onGameOver);
-                break;
-            default:
-                break;
-        }
+   
     }
 
     // Предоставляет картам необходимый доступ к объектам игры.
@@ -77,29 +45,59 @@ const Game = function() {
     // - position - позиция текущей карты, начиная слева, считается с 0
     // - updateView - обновляет вид всех объектов игры,
     //   полезен когда действие некоторой карты повлияло на множество объектов
-    Game.prototype.getContextForCard = function (position) {
+
+    getContextForCard(position) {
         return {
             currentPlayer: this.currentPlayer,
             oppositePlayer: this.oppositePlayer,
             position: position,
             updateView : () => this.updateView(),
         }
-    };
+}
 
-    // Обновляет вид всех объектов игры.
-    Game.prototype.updateView = function () {
-        this.currentPlayer.updateView();
-        this.oppositePlayer.updateView();
-    };
 
-    function getWinner(game) {
-        if (hasNoPower(game.oppositePlayer)) {
-            return game.currentPlayer;
-        }
-        if (hasNoPower(game.currentPlayer)) {
-            return game.oppositePlayer;
-        }
-        return null;
+// Выполняет действия для некоторой стадии хода.
+// Переход к следующей стадии идет через самовызов в колбэке.
+function playStaged(game, stage, onGameOver) {
+    switch (stage) {
+        case 0:
+            game.currentPlayer.playNewCard(() => playStaged(game, 1, onGameOver));
+            break;
+        case 1:
+            game.currentPlayer.applyCards(() => playStaged(game, 2, onGameOver));
+            break;
+        case 2:
+            game.oppositePlayer.removeDeadAndCompactTable(() => playStaged(game, 3, onGameOver));
+            break;
+        case 3:
+            game.currentPlayer.removeDeadAndCompactTable(() => playStaged(game, 4, onGameOver));
+            break;
+        case 4:
+            const winner = getWinner(game);
+            if (winner) {
+                onGameOver(winner);
+                return;
+            }
+            playStaged(game, 5, onGameOver);
+            break;
+        case 5:
+            changePlayer(game);
+            playStaged(game, 0, onGameOver);
+            break;
+        default:
+            break;
+    }
+}
+function getWinner(game) {
+    if (hasNoPower(game.oppositePlayer)) {
+        return game.currentPlayer;
+    }
+
+    if (hasNoPower(game.currentPlayer)) {
+        return game.oppositePlayer;
+    }
+
+    return null;
     }
 
     function hasNoPower(player) {
@@ -123,13 +121,10 @@ const Game = function() {
         const result = [...array];
         result.reverse();
         return result;
-    }
+}
 
-    function compareRandom() {
-        return Math.random() - 0.5;
-    }
-
-    return Game;
-}();
+function compareRandom() {
+    return Math.random() - 0.5;
+}
 
 export default Game;
