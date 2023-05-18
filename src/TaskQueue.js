acbfd17161dc2fdf89427a0f523ddcc54eebaa62
Alexey Bolotov <stealthfear@gamil.com>
2023-05-18 14:14:32 +0500

    Task 1/10: Переписан TaskQueue с IIFE на class

--- src/TaskQueue.js
+++ src/TaskQueue.js
@@ -1,10 +1,10 @@
-const TaskQueue = function() {
-    function TaskQueue() {
-        this.tasks = [];
+class TaskQueue {
+    constructor() {
+        this.tasks = []
         this.running = false;
     }
 
-    TaskQueue.prototype.push = function(run, dispose, duration) {
+    push(run, dispose, duration) {
         if (duration === undefined || duration === null) {
             this.tasks.push({runAndContinue: run, dispose});
         } else {
@@ -18,14 +18,14 @@ const TaskQueue = function() {
                 dispose
             });
         }
-        runNextTask(this);
-    };
+        this.runNextTask(this);
+    }
 
-    TaskQueue.prototype.continueWith = function(action) {
+    continueWith(action) {
         this.push(action, null, 0);
-    };
+    }
 
-    function runNextTask(taskQueue) {
+    runNextTask(taskQueue) {
         if (taskQueue.running || taskQueue.tasks.length === 0) {
             return;
         }
@@ -39,17 +39,15 @@ const TaskQueue = function() {
                     taskQueue.running = false;
 
                     setTimeout(() => {
-                        runNextTask(taskQueue);
+                        this.runNextTask(taskQueue);
                     });
                 });
             }, 0);
-        }
-        else {
-            runNextTask(taskQueue);
+        } else {
+            this.runNextTask(taskQueue);
         }
     }
+}
 
-    return TaskQueue;
-}();
 
 export default TaskQueue;
